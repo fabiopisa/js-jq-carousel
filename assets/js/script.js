3c900faa1cca39de4863b3976293db2ea6e533b4
fabiopisa <pisanellifabio@gmail.com>
2021-04-20 15:40:22 +0200

    commit html e css finito

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  //creo un ciclo che mi inserisca tanti circle quante sono le immagini
+  /* var arrImg = $(".contain-img");
+  var arrCircle = $(".circle-scroll");
+  for(var i =0; i<arrImg.lenght; i++){
+    arrCircle.html('<i class="far fa-circle"></i>');
+  } */
+
+})
